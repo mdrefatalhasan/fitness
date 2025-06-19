99d234ca99f0b7872fbfcf6a6f15bc2dcb14402c
Refat al hasan <muhammadrefatalhasankaif@gmail.com>
2025-06-20 00:41:18 +0600

    hero image animation done

---
+++ script.js
@@ -0,0 +1,37 @@
+const images = [
+  'images/slide/1.png',
+  'images/slide/2.png',
+  'images/slide/3.png',
+  'images/slide/4.png',
+  'images/slide/5.png',
+  'images/slide/6.png',
+  'images/slide/7.png',
+  'images/slide/8.png',
+  'images/slide/9.png',
+  'images/slide/10.png',
+  'images/slide/11.png',
+  'images/slide/12.png',
+  'images/slide/13.png',
+  'images/slide/14.png',
+  'images/slide/15.png',
+  'images/slide/16.png',
+];
+
+let current = 0;
+const bgLayer = document.getElementById('bg-layer');
+
+bgLayer.style.backgroundImage = `url(${images[current]})`;
+
+current++;
+
+function changeBackground() {
+  bgLayer.style.opacity = '0';
+
+  setTimeout(() => {
+    bgLayer.style.backgroundImage = `url(${images[current]})`;
+    bgLayer.style.opacity = '1';
+    current = (current + 1) % images.length;
+  }, 1000);
+}
+
+setInterval(changeBackground, 3000);
